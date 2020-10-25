d515e7d69049a2c587f69562d827637948d8fd50
Bruno Van Den Bossche <bruno.vandenbossche@gmail.com>
2020-10-25 21:38:31 +0100

    Add delays
    Add some delays to prevent that the services are being started too soon

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -3,8 +3,12 @@
 #start the ShairPlay server with the original script
 /start.sh $@ &
 
+sleep 5
+
 #start icecast streaming server
 icecast -c /ShairStream/Icecast/icecast.xml -b
+
+sleep 5
 
 #create stream
 cat /tmp/shairport-sync-audio | lame -b 320 -r - | ezstream -r -c /ShairStream/Ezstream/ezstream-stdin.xml
