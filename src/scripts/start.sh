6b787263cb2a80ede72fa07163cf05404099f208
Bruno Van Den Bossche <bruno.vandenbossche@gmail.com>
2020-10-25 21:15:12 +0100

    Fixing config issues
    Fixing some issues in the configuration and setup I missed in final testing

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #start the ShairPlay server with the original script
-/start.sh $@
+/start.sh $@ &
 
 #start icecast streaming server
-icecast -c /ShairStream/assets/Icecast/icecast.xml -b
+icecast -c /ShairStream/Icecast/icecast.xml -b
 
 #create stream
-cat /tmp/shairport-sync-audio | lame -b 320 -r - | ezstream -r -c /ShairStream/assets/Ezstream/ezstream-stdin.xml
+cat /tmp/shairport-sync-audio | lame -b 320 -r - | ezstream -r -c /ShairStream/Ezstream/ezstream-stdin.xml
